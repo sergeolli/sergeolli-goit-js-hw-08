131b7766e8f0c5c95e7d6f70441b38c4ce0c0275
sergeolli <sergeyoleynik301087@gmail.com>
2021-12-22 16:15:57 +0200

    дз.1.1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,44 +1,34 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Change code below this line
+
 const galleryContainer = document.querySelector('.gallery');
 const galleryMarkup = createGalleryMarkup(galleryItems)
 galleryContainer.innerHTML = galleryMarkup;
 
-galleryContainer.addEventListener('click', onreateGalleryMarkup)
-
 function createGalleryMarkup(galleryItems) {
     return galleryItems
      .map(({ preview, original, description }) => {
-            return `
+       return `
 <div class="gallery__item">
 <a class="gallery__link" href='${original}'>
 <img class="gallery__image"
 src='${preview}'
-data-source='${original}'
 alt='${description}'/>
 </a>
 </div>`;
-      }).join('');
-}
+       }).join('');
+};
+ galleryContainer.addEventListener('click', onreateGalleryMarkup)
 
 function onreateGalleryMarkup(e) {
-  e.preventDefault();
-  if (!e.target.dataset.source) {
-    return 
-  }
-  const modal = basicLightbox.create(
-`<img 
-src= ${e.target.dataset.source}
->`)
-  modal.show()
-   
-  document.addEventListener("keydown", closeModal)
-  function closeModal(e) {
-    if (e.code === "Escape") { 
-    }
-    modal.close() 
-  }
-  document.removeEventListener("keydown", closeModal)
-}
-console.log(galleryItems);
+       e.preventDefault()
+       galleryContainer.removeEventListener('click', onreateGalleryMarkup)
+ const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+ })
+};
