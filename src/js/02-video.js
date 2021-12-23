3bae84ac05eba0e99266dd970862f70b2dac5820
sergeolli <sergeyoleynik301087@gmail.com>
2021-12-24 00:07:47 +0200

    дз.8.2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,18 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('timeupdate', throttle((e) => {
+    localStorage.setItem('videoplayer-current-time', JSON.stringify(e.seconds));
+}, 1000));
+player.setCurrentTime(JSON.parse(localStorage.getItem('videoplayer-current-time'))); 
+    
+     
+    
+
+
+
+
 
