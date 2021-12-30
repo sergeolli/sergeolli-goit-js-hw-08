c85ecce9f6a7f4cc1c0cf8d872b628a1dfce6443
sergeolli <sergeyoleynik301087@gmail.com>
2021-12-30 23:33:57 +0200

    дз.8.3 правки 3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,48 +1,114 @@
 import throttle from 'lodash.throttle';
 
 const refs = {
-    form: document.querySelector('.feedback-form'),
-    formTextarea: document.querySelector('.feedback-form textarea'),
-    formInput: document.querySelector('.feedback-form input'), 
+form: document.querySelector('.feedback-form'),
+formTextarea: document.querySelector('.feedback-form textarea'),
+formInput: document.querySelector('.feedback-form input'),  
 };
-
-const formData = {};
 
 const STOREG_KEY = 'feedback-form-state';
 
-refs.form.addEventListener('submit', onFormSubmit);
+saveСheckValues();
 
-popularTextarea();
+refs.form.addEventListener('submit', onFormSabmit);
+refs.form.addEventListener('input', throttle(onFormInput,500));
 
-refs.form.addEventListener('input', throttle(e => {
+function onFormInput(e) {
+    const formElements = e.currentTarget.elements;
+    const email = formElements.email.value;
+    const message = formElements.message.value;
+    const formDataSave = {
+        email, message
+    };
+    localStorage.setItem(STOREG_KEY, JSON.stringify(formDataSave));
+    const formData = JSON.parse(localStorage.getItem(STOREG_KEY)); 
+    refs.formTextarea.value = formData.message
+    refs.formInput.value = formData.email
+}
+
+function onFormSabmit(e) {
+    e.preventDefault();
     
-    formData[e.target.name] = e.target.value; 
-    localStorage.setItem(STOREG_KEY, JSON.stringify(formData)) 
- 
-},500));
-
-function onFormSubmit(e) {
-    e.preventDefault()  
-    localStorage.removeItem(STOREG_KEY) 
+    const allValues = {
+        email: refs.formInput.value,
+        message: refs.formTextarea.value
+    };
     if (refs.formTextarea.value === '' || refs.formInput.value === '' ) {
-        alert('All fields must be completed!');   
+     alert('All fields must be completed!');   
     } else {
-        formData.email = refs.formTextarea.value
-        formData.message = refs.formInput.value
+        localStorage.removeItem(STOREG_KEY)
+        console.log(allValues)
         e.currentTarget.reset();
-        console.log(formData)
-         formData.message = ''
-       formData.email =''
-    }    
-};
+     }   
+}
 
-function popularTextarea() {
-    const saveValueInput = JSON.parse(localStorage.getItem(STOREG_KEY));
-    if (saveValueInput) {
-        refs.formTextarea.value = saveValueInput.message || ''
-        refs.formInput.value = saveValueInput.email || ''
-    } 
+function saveСheckValues() {
+   const saveValueInput = JSON.parse(localStorage.getItem(STOREG_KEY));
+   if (saveValueInput) {
+       refs.formTextarea.value = saveValueInput.message 
+       refs.formInput.value = saveValueInput.email 
+   }    
 };
-   
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+//const refs = {
+  //  form: document.querySelector('.feedback-form'),
+   // formTextarea: document.querySelector('.feedback-form textarea'),
+   // formInput: document.querySelector('.feedback-form input'), 
+//};
+
+//const formData = {} 
+ 
+//const STOREG_KEY = 'feedback-form-state';
+//popularTextarea();
+
+
+//refs.form.addEventListener('submit', onFormSubmit);
+
+//refs.form.addEventListener('input', throttle(e  =>  {
+  //  formData[e.target.name] = e.target.value; 
+  //  localStorage.setItem(STOREG_KEY, JSON.stringify(formData))
+ //console.log(formData)
+//}, 500));
+
+
+//function onFormSubmit(e) {
+  //  e.preventDefault()
+   //     formData.email = refs.formInput.value || '' 
+   //     formData.message = refs.formTextarea.value || ''
+          
+    //if (refs.formTextarea.value === '' || refs.formInput.value === '' ) {
+    //    alert('All fields must be completed!');   
+    //} else {
+    //   localStorage.removeItem(STOREG_KEY)
+     //   e.currentTarget.reset();
+     //   console.log(formData)
+      //  formData.message =  ''
+      //  formData.email = '' 
+ //   }   
+      
+//};
+
+//function popularTextarea() {
+  //  const saveValueInput = JSON.parse(localStorage.getItem(STOREG_KEY));
+  //  if (saveValueInput) {
+    //      refs.formTextarea.value = saveValueInput.message || ''
+    //    refs.formInput.value = saveValueInput.email || ''
+   // } 
+    
+//};
+  
  
 
